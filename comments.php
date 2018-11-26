3b5c265b249a159cbfdd72760ac5aea4775c631b
Joshua Tzucker <joshua.tz@gmail.com>
2018-11-26 09:53:33 -0800

    added in wordpress commenting system - starting to set up

---
+++ comments.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying comments
+ */
+?>
+<div id="comments" class="comments-area">
+    <div id="wordpress-commenting-wrapper-wrapper">
+        <?php include('partials/wordpress-commenting.php'); ?>
+    </div>
+    <div id="disqus-commenting-wrapper-wrapper">
+        <?php include('partials/disqus-commenting.php'); ?>
+    </div>
+</div>
--- partials/disqus-commenting.php
+++ partials/disqus-commenting.php
@@ -1,5 +1,58 @@
 <?php
 /**
- * Disqus commenting section
+ * Partial - Disqus commenting section
  */
 ?>
+<?php
+/**
+ * Preprocessing - check for valid setting
+ */
+$hasValidDisqus = false;
+$hasNoDisqus = false;
+$disqusCustomSubdomain = get_option('jtzwp_settings')['jtzwp_disqus_subdomain'];
+if (isset($disqusCustomSubdomain) && preg_match('/[^\.]+\.disqus\.com/',$jtzwp_disqus_subdomain)){
+    $hasValidDisqus = true;
+}
+else if (!isset($disqusCustomSubdomain) || $disqusCustomSubdomain===''){
+    $hasNoDisqus = true;
+}
+?>
+
+<div id="disqusCommentWrapper" style="width:90%; margin-left:5%">
+    <?php if($hasValidDisqus): ?>
+    <div id="disqus_thread"></div>
+    <?php elseif(!$hasNoDisqus): ?>
+    <div>Invalid Disqus Config</div>
+    <?php else: ?>
+    <!-- No disqus configured -->
+    <?php endif; ?>
+</div>
+<script>
+
+<?php if($hasValidDisqus): ?>
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+/*
+var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+    
+var disqus_config = function () {
+    // this.page.url - should be page's canonical URL
+    this.page.url = (document.location.origin + document.location.pathname);
+    // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    //this.page.identifier = PAGE_IDENTIFIER; 
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = '//<?php echo $disqusCustomSubdomain;?>/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> 
+<?php endif; ?>
