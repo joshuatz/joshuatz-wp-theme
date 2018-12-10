f79c78d8372bd9f125f8029d2f9dd1af710bd314
Joshua Tzucker <joshua.tz@gmail.com>
2018-12-10 12:44:29 -0800

    cleanup

--- footer.php
+++ footer.php
@@ -3,13 +3,11 @@
  * Footer
  */
 ?>
-<a class="waves-effect waves-light btn modal-trigger" href="#businessCardMaterializeModal">Business Card</a>
 <div id="businessCardMaterializeModal" class=" businessCardMaterializeModal modal" data-opacity="0.9">
     <div class="modal-content">
         <?php get_template_part('partials/business-card-materialize'); ?>
     </div>
 </div>
-<?php get_template_part('partials/business-card-materialize'); ?>
 <?php wp_footer(); ?>
 </body>
 </html>
