c995085d91294d1a173c02955dedcab94eeb8ca7
Joshua Tzucker <joshua.tz@gmail.com>
2019-01-24 23:59:53 -0800

    empty template file

---
+++ tag.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tag Archives - this will trigger across all posts types as the generic tag archive template file
+ */
+get_header(); ?>
+
+<?php
+    global $jtzwpHelpers;
+?>
+<div id="main">
+</div>
+
+<?php get_footer(); ?>
